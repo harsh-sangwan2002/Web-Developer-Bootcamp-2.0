9ba4e30cbda021ff74e49e780a17159d51acafa0
Harsh Sangwan <hsangwan2002@gmail.com>
2025-03-21 16:21:54 +0530

    functions done

---
+++ JavaScript/Functions/app.js
@@ -0,0 +1,20 @@
+function singSong() {
+    console.log("DO");
+    console.log("RE");
+    console.log("MI");
+}
+singSong();
+singSong();
+singSong();
+
+function greet(firstName, lastName) {
+    console.log(`Hello, ${firstName} ${lastName}!`);
+}
+greet("Tim", "Cook");
+greet("Billy", "Bob");
+
+function add(x,y){
+    return x+y;
+}
+console.log(add(12, 16));
+console.log(add(12, 16) + 100);
