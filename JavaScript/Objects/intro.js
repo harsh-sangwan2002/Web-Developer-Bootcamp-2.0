e188a8ac3da6b63185644d3e17c31ffbc79dc73e
Harsh Sangwan <hsangwan2002@gmail.com>
2025-03-21 15:50:07 +0530

    objects done

---
+++ JavaScript/Objects/intro.js
@@ -0,0 +1,16 @@
+const person = {
+    firstName: 'John',
+    lastName: 'Doe',
+    age: 25,
+    address: {
+        street: '123 Main St',
+        city: 'New York',
+        state: 'NY'
+    }
+}
+
+console.log(person.firstName); // John
+console.log(person.address.city); // New York
+
+const midterms = { danielle: 96, thomas: 78 };
+console.log(midterms)
---
+++ JavaScript/Objects/nesting.js
@@ -0,0 +1,5 @@
+const coments = [
+    { username: "Tammy", text: "lololol", votes: 9 },
+    { username: "FishBoi", text: "glub glub", votes: 123 },
+    { username: "Haha", text: "hahahah", votes: 3 }
+]
