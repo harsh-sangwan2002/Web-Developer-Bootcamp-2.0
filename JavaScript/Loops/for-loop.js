ef7a376e4d8418fbf55be24c55e629b735d936c9
Harsh Sangwan <hsangwan2002@gmail.com>
2025-03-21 16:04:04 +0530

    loops done

---
+++ JavaScript/Loops/for-loop.js
@@ -0,0 +1,17 @@
+for (let i = 0; i <= 10; i++) {
+    console.log(i);
+}
+
+for (let i = 0; i <= 20; i += 2) {
+    console.log(i);
+}
+
+// for (let i = 0; i < 10; i--) {
+//     console.log(i);
+// }
+
+// for-of loop
+const subreddits = ['cringe', 'books', 'chickens', 'funny', 'pics', 'soccer'];
+for (let reditts of subreddits) {
+    console.log(reditts);
+}
---
+++ JavaScript/Loops/objects-iterator.js
@@ -0,0 +1,10 @@
+const testScores = {
+    kennan: 90,
+    kim: 89,
+    kassey: 99,
+    kia: 100
+}
+
+for (let score in testScores) {
+    console.log(score);
+}
