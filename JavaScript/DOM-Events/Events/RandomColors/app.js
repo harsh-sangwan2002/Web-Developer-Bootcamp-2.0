97d511402ca666bd44cf5b3642f8627a1eea2e4c
harsh-sangwan2002 <hsangwan2002@gmail.com>
2025-03-24 14:27:50 +0530

    events done

--- JavaScript/DOM-Events/Events/RandomColors/app.js
+++ JavaScript/DOM-Events/Events/RandomColors/app.js
@@ -1,16 +1,17 @@
+const heading = document.querySelector('h1');
 const button = document.querySelector('button');
-const h1 = document.querySelector('h1');
 
-button.addEventListener('click', function () {
-    const newColor = makeRandColor();
-    document.body.style.backgroundColor = newColor;
-    h1.innerText = newColor;
-})
+button.addEventListener('click', () => {
 
-const makeRandColor = () => {
+    const color = randomColor();
+    document.body.style.backgroundColor = color;
+    heading.innerText = color;
+});
+
+const randomColor = () => {
     const r = Math.floor(Math.random() * 255);
     const g = Math.floor(Math.random() * 255);
     const b = Math.floor(Math.random() * 255);
-    return `rgb(${r}, ${g}, ${b})`;
-}
 
+    return `rgb(${r},${g},${b})`;
+}
